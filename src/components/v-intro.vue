b8f2cefaabc1d2443b582d23ee8df912bcea4611
River <riveriswild@icloud.com>
2022-08-03 00:00:27 +0300

    Introduction added

---
+++ src/components/v-intro.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="full">
+
+    <div class="header">
+        <h1 class="title">Nati Tattoo</h1>
+
+        <div class="header__button">
+            <div class="header__buttonContainer widthContainer">
+                <a href="#" class="btn btn-aqua btn-mid btn-withArrowLeft"><span>Про нати</span></a>
+            </div>
+        </div>
+    </div>
+
+                <!--bordered content-->
+
+            </div>
+</template>
+
+<script>
+export default {
+    name: 'v-intro'
+
+}
+</script>
+
+<style>
+
+</style>
