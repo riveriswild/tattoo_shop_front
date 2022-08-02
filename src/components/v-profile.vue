d42389972a311fff0fad3931d861939e42fc9bd3
River <riveriswild@icloud.com>
2022-08-03 00:01:03 +0300

    Profile added

---
+++ src/components/v-profile.vue
@@ -0,0 +1,52 @@
+<template>
+  
+    <div class="view">
+        <div class="viewContent" style="left: -600vw;">
+            <!--СЛАЙД-->
+            <div class="viewContent__item">
+
+                сюда галерею
+
+            </div>
+            <!--СЛАЙД-->
+
+            <!--СЛАЙД-->
+            <div class="viewContent__item">
+                <!--bordered content-->
+                <div class="content widthContainer">
+                    <div class="contentTitle">О мастере</div>
+
+                    <div class="contentCols">
+                        <div class="contentCols__item">
+                            <img src="../assets/img/photo1.png" alt="Nati">
+                        </div>
+                        <div class="contentCols__item">
+                            <div class="text">
+                                <p>С точки зрения банальной эрудиции, каждый индивидуум, критически мотивирующий абстракцию, не может игнорировать критерии утопического субъективизма, концептуально интерпретируя общепринятые дефанизирующие поляризаторы.</p>
+                                <p>Критически мотивирующий абстракцию, не может игнорировать критерии утопического субъективизма, концептуально интерпретируя общепринятые дефанизирующие поляризаторы, поэтому консенсус.</p>
+
+                                <p>Общепринятые дефанизирующие поляризаторы, поэтому консенсус, достигнутый диалектической материальной классификацией всеобщих мотиваций в парадигматических связях предикатов, решает проблему усовершенствования формирующих геотрансплантационных квазипузлистатов всех кинетически коррелирующих аспектов.</p>
+                            </div>
+                        </div>
+                        <div class="contentCols__item">
+                            <img src="../assets/img/photo2.png" alt="Tattoo">
+                        </div>
+                    </div>
+
+
+                </div>
+                </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'v-profile'
+
+}
+</script>
+
+<style>
+
+</style>
