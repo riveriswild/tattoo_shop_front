905338be2d493c41b045c30c0af1cc25f289073e
River <riveriswild@icloud.com>
2022-08-10 03:30:55 +0300

    Add reviews page

--- src/components/v-reviews.vue
+++ src/components/v-reviews.vue
@@ -3,97 +3,105 @@
                 <!--bordered content-->
                 <div class="content widthContainer">
                     <div class="contentTitle">Отзывы</div>
-
+                   
                     <div class="review">
+                       
                         <div class="review__content">
-
-                            <div class="review__item">
-
-                                <div class="review__image">
-                                    <img src="img/review-photo.png" class="review__photo">
-                                    <img src="img/review-tattoo.png" class="review__tattoo">
-                                </div>
-
-                                <div class="review__name">София</div>
-
-                                <div class="review__text">
-                                    Классная татуировка, все круто, ваще збс, все понравилось, ура ура короче отзыв тут лалала.
-                                </div>
-
-                            </div>
-                            <div class="review__item">
-
-                                <div class="review__image">
-                                    <img src="img/review-photo.png" class="review__photo">
-                                    <img src="img/review-tattoo.png" class="review__tattoo">
-                                </div>
-
-                                <div class="review__name">София</div>
-
-                                <div class="review__text">
-                                    Классная татуировка, все круто, ваще збс, все понравилось, ура ура короче отзыв тут лалала.
-                                </div>
-
-                            </div>
-                            <div class="review__item">
-
-                                <div class="review__image">
-                                    <img src="img/review-photo.png" class="review__photo">
-                                    <img src="img/review-tattoo.png" class="review__tattoo">
-                                </div>
-
-                                <div class="review__name">София</div>
-
-                                <div class="review__text">
-                                    Классная татуировка, все круто, ваще збс, все понравилось, ура ура короче отзыв тут лалала.
-                                </div>
-
-                            </div>
-                            <div class="review__item">
-
-                                <div class="review__image">
-                                    <img src="img/review-photo.png" class="review__photo">
-                                    <img src="img/review-tattoo.png" class="review__tattoo">
-                                </div>
-
-                                <div class="review__name">София</div>
-
-                                <div class="review__text">
-                                    Классная татуировка, все круто, ваще збс, все понравилось, ура ура короче отзыв тут лалала.
-                                </div>
-
-                            </div>
-                            <div class="review__item">
-
-                                <div class="review__image">
-                                    <img src="img/review-photo.png" class="review__photo">
-                                    <img src="img/review-tattoo.png" class="review__tattoo">
-                                </div>
-
-                                <div class="review__name">София</div>
-
-                                <div class="review__text">
-                                    Классная татуировка, все круто, ваще збс, все понравилось, ура ура короче отзыв тут лалала.
-                                </div>
-
-                            </div>
-                        </div>
+                            
+                            <v-review-item
+                            v-for="review in REVIEWS"
+                            :key="review.client_name"
+                            :clt_review="review"
+                            />
                     </div>
 
-
-                </div>
-                <!--bordered content-->
-
+                </div> 
+                
+            </div>
             </div>
 </template>
+ 
 
 <script>
+import vReviewItem from "./v-review-item.vue"
+import { mapActions, mapGetters } from 'vuex'
 export default {
-    name: 'v-reviews'
+    name: 'v-reviews',
+    components: {
+        vReviewItem
+    },
+        props: {},
+        data() {
+            return {
+                smth: {},
+            }
+        },
+        computed: {
+            ...mapGetters([
+                'REVIEWS'
+            ])
+        },
 
+        methods: {
+            ...mapActions([
+                'GET_REVIEWS_FROM_API'
+            ])
+        },
+    async created() {
+         await this.GET_REVIEWS_FROM_API()
+    }
 }
+
 </script>
 
 <style>
+
+.review {
+    overflow-x: auto;
+    overflow-y: auto;
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
+.review::-webkit-scrollbar {
+  display: none;
+}
+
+.review__content {
+    display: flex;
+}
+
+.review__item {
+    flex-basis: 450px;
+    flex-shrink: 0;
+    flex-grow: 0;
+    margin-right: 100px;
+    margin-bottom: 20px;
+}
+
+.review__image {
+    font-size: 0;
+    margin-bottom: 33px;
+}
+
+.review__image img {
+    width: 45%;
+}
+
+.review__image img + img {
+    margin-left: 19px;
+}
+
+.review__name {
+    font-size: 24px;
+    text-transform: uppercase;
+    font-weight: 600;
+    margin-bottom: 18px;
+}
+
+.review__text {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 21px;
+}
 
 </style>
