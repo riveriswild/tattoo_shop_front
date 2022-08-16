9005743fcd0b36e0d2cf8b4645c2c7797a697dd7
River <riveriswild@icloud.com>
2022-08-16 23:06:40 +0300

    Sorting in gallery (in process)

--- src/components/v-gallery.vue
+++ src/components/v-gallery.vue
@@ -1,16 +1,28 @@
 <template>
 
   <div class="viewContent__item">
-    
                 <!--bordered content-->
                 <div class="content widthContainer">
                     <div class="contentTitle">Галерея работ</div>
 
                     <div class="contentCols contentCols--gallery">
+
+                        <div class="rbButtons">
+                            <div @click="showNew" class="rbButtons__item active"><a href="#" class="btn btn-aqua btn-mid">Свежие работы</a></div>
+                            <div @click="showOld" class="rbButtons__item"><a href="#" class="btn btn-aqua btn-mid">Зажившие</a></div>
+                            <div @click="showSketch" class="rbButtons__item"><a href="#" class="btn btn-aqua btn-mid">Эскизы</a></div>
+                        </div>
+
                         <div class="contentCols__item">
                             <img src="img/photo2.png" class="gallery__mainpic" alt="T1">
                         </div>
                         <div class="contentCols__item gallery">
+                            <div :selected="selected"
+            :options="status" v-for="item in this.sortedWorks" v-bind:key="item.id">
+                                    <span class="gallery__item"><img :src="item.image"></span>
+                                    <span class="gallery__item active"><img :src="item.image"></span>
+                            </div>
+
 
                         </div>
 
@@ -22,6 +34,7 @@
 
             </div>
 </template>
+
 
 <script>
 import { mapActions, mapGetters} from 'vuex';
@@ -46,6 +59,11 @@ export default {
                 'WORKS'
             ]),
         },
+          watch: {
+    // эта функция запускается при любом изменении вопроса
+            // selected: this.sortByStatus()
+    
+  },
 
         methods: {
             ...mapActions([
@@ -56,11 +74,23 @@ export default {
                 let vm = this
                 this.sortedWorks = [...this.WORKS]
                 this.sortedWorks = this.sortedWorks.filter(function(item) {
-                    vm.selected = status.name
-                    return item.status === status.name
+                    console.log(item)
+                    vm.selected = status.value
+                    console.log(vm.selected)
+                    // console.log(item.image_status)
+                    return item.image_status === status.value
                 })
+                },
+                
+                showOld(){
+                    return this.selected = 'new'
+                },
+                showNew(){
+                    return this.selected = 'old'
+                },
+                showSketch(){
+                    return this.selected = 'sketch'
                 }
-
         },
     async created() {
         await this.GET_WORKS_FROM_API()
@@ -70,5 +100,40 @@ export default {
 </script>
 
 <style>
+.contentCols--gallery .contentCols__item {
+    flex-basis: 50%;
+}
+
+.contentCols--gallery .contentCols__item:first-child {
+    text-align: right;
+    padding: 5px 0;
+}
+
+.gallery__mainpic {
+    height: 100%;
+}
+
+.gallery {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: flex-start;
+    align-items: stretch;
+}
+
+.gallery__item {
+    flex-basis: 100px;
+    height: 100px;
+    overflow: hidden;
+    margin: 5px;
+    cursor: pointer;
+}
+
+.gallery__item.active, .gallery__item:hover {
+    opacity: 0.50;
+}
+
+.gallery__item img {
+    width: 120%;
+}
 
 </style>
