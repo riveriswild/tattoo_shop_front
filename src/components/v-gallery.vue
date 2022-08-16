0b53a0b05f23d069384951f4845b0d0e5c83d76d
River <riveriswild@icloud.com>
2022-08-17 01:08:35 +0300

    Fix gallery mechanics

--- src/components/v-gallery.vue
+++ src/components/v-gallery.vue
@@ -13,9 +13,9 @@
                             <div @click="showSketch" class="rbButtons__item"><a href="#" class="btn btn-aqua btn-mid">Эскизы</a></div>
                         </div>
 
-                        <div class="contentCols__item">
-                            <img src="img/photo2.png" class="gallery__mainpic" alt="T1">
-                        </div>
+                      <!--  <div class="contentCols__item">
+                            <img :src="this.sortedWorks[0].image" class="gallery__mainpic" alt="T1">
+                        </div>-->
                         <div class="contentCols__item gallery">
                             <div v-for="item in this.sortedWorks" v-bind:key="item.id">
                                     <span class="gallery__item"><img :src="item.image"></span>
@@ -43,11 +43,6 @@ export default {
         props: {},
         data() {
             return {
-                // status: [
-                //     {name: 'Свежие работы', value: 'new'},
-                //     {name: 'Зажившие работы', value: 'old'},
-                //     {name: 'Эскизы', value: 'sketch'},
-                // ],
                 sortedWorks: [],
                 selected: 'new',
             }
@@ -56,14 +51,7 @@ export default {
             ...mapGetters([
                 'WORKS'
             ]),
-        
-            //         filteredWorks() {
-            //     if (this.sortedWorks.length) {
-            //         return this.sortedWorks
-            //     } else {
-            //         return this.WORKS
-            //     }
-            // }
+
         },
         watch: {
             selected: {
@@ -106,6 +94,48 @@ export default {
 </script>
 
 <style>
+.widthContainer {
+    margin: 0 auto;
+}
+
+.widthContainer {
+    max-width: 1452px;
+}
+
+@media only screen  and (max-width: 1000px) {
+    .widthContainer {
+        max-width: 1000px;
+    }
+}
+
+.contentTitle {
+    font-size: 32px;
+    color: #000000;
+    font-family: 'Montserrat', sans-serif;
+    line-height: 40px;
+    text-transform: uppercase;
+    margin-bottom: 20px;
+
+}
+
+.contentCols {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.contentCols--gallery {
+    align-items: stretch;
+}
+
+.contentCols.top {
+    align-items: flex-start;
+}
+
+.contentCols__item + .contentCols__item {
+    padding-left: 20px;
+}
+
 .contentCols--gallery .contentCols__item {
     flex-basis: 50%;
 }
@@ -141,5 +171,45 @@ export default {
 .gallery__item img {
     width: 120%;
 }
+.btn-aqua {
+    background-color: #75cfd0;
+    color: #ffffff;
+}
 
+.btn-aqua:hover {
+    background-color: #82e1e2;
+    
+}
+
+.btn-aqua:active {
+    background-color: #2cd0c8;
+}
+.rbButtons {
+    position: absolute;
+    left:0;
+    width: 275px;
+}
+
+.rbButtons.right {
+    left: auto;
+    right: 0;
+    text-align: right;
+}
+
+.rbButtons__item {
+    margin-bottom: 20px;
+}
+
+.rbButtons__item .btn {
+    transition: all 0.3s 0s ease-in-out;
+}
+
+.rbButtons .btn-mid {
+    width: 239px;
+    white-space: nowrap;
+}
+
+.rbButtons__item.active .btn-mid {
+    width: 275px;
+}
 </style>
